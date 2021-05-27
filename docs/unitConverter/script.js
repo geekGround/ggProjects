ac8b369ac0bc810803bf91d28c1e67935ac6339c
Thalles Yuri <thalles.yuri1995@gmail.com>
2021-05-26 23:03:00 -0700

    Add CSS and part of the DOM

--- docs/unitConverter/script.js
+++ docs/unitConverter/script.js
@@ -1,11 +1,32 @@
+const dropdownBtns = document.querySelectorAll('.dropdown-btn');
+const dropdownContents = document.querySelectorAll('.dropdown-content');
+const units = document.querySelectorAll('.dropdown-content p');
+
+for (let unit of units) {
+  unit.addEventListener('click', () => {
+    unit.parentElement.previousElementSibling.innerText = unit.innerText;
+    unit.parentElement.classList.remove('active');
+    let unitID = unit.id;
+    //Create Units
+  });
+}
+
+for (let btn of dropdownBtns) {
+  btn.addEventListener('click', () => {
+    btn.nextElementSibling.classList.toggle('active');
+  });
+}
+
 let myData = {
   mass: {
+    units: ['mg', 'g', 'kg', 'ton'],
     mg: 1 / 1000, //1mg = 1/1000g,
     g: 1, //1g = 1g,
     kg: 1000, //1kg = 1000g,
     ton: 1000000, //1ton = 1000000g
   },
   temperature: {
+    units: ['celsius', 'fahrenheit', 'kelvin'],
     celsius: {
       fahrenheit: (valor) => valor * 1.8 + 32,
       kelvin: (valor) => valor + 273.15,
@@ -20,12 +41,14 @@ let myData = {
     },
   },
   length: {
+    units: ['mm', 'cm', 'm', 'km'],
     mm: 1 / 1000,
     cm: 1 / 100,
     m: 1,
     km: 1000,
   },
   time: {
+    units: ['s', 'min', 'h', 'day', 'week', 'month'],
     s: 1,
     min: 60,
     h: 3600,
@@ -34,7 +57,9 @@ let myData = {
     month: 2629600,
   },
   volume: {
+    units: ['m3', 'ml', 'l', 'dm3', 'cm3'],
     m3: 1,
+    ml: 1000000,
     l: 1000,
     dm3: 1000,
     cm3: 1000000,
